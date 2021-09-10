f4df4f8a199396c5753f1a62ae780cc451e8e221
theja <tt@theja.org>
2021-09-09 19:09:05 -0500

    cloud function fixed

--- cloud_function_example/main.py
+++ cloud_function_example/main.py
@@ -19,10 +19,13 @@ def predict(request):
 
     model = get_model(model_params[0], model_params[1])
 
+    request_json = request.get_json()
     if "x" in request.args:
         try:
             return jsonify({'input': request.args['x'], 'prediction': model(float(request.args['x']))})
         except:
             pass
-
-    return jsonify({'success': 'false', 'message': 'Input x was not passed correctly.'})
+    elif request_json and 'x' in request_json:
+        return jsonify({'input': request_json['x'], 'prediction': model(float(request_json['x']))})
+    else:
+        return jsonify({'success': 'false', 'message': 'Input x was not passed correctly.'})
