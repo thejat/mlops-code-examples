2344388e51c780a8a7e14f125a42f25f22a30e88
Theja <tt@theja.org>
2024-01-10 14:40:56 -0600

    minor

---
+++ airflow_pipeline_example/airflow_install.sh
@@ -0,0 +1,31 @@
+# airflow needs a home, ~/airflow is the default,
+# but you can lay foundation somewhere else if you prefer
+# (optional)
+export AIRFLOW_HOME=~/airflow
+
+AIRFLOW_VERSION=2.1.4
+PYTHON_VERSION="$(python --version | cut -d " " -f 2 | cut -d "." -f 1-2)"
+# For example: 3.6
+CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
+# For example: https://raw.githubusercontent.com/apache/airflow/constraints-2.1.4/constraints-3.6.txt
+pip install "apache-airflow==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
+
+# initialize the database
+airflow db init
+
+airflow users create \
+	    --username admin \
+	        --firstname Peter \
+		    --lastname Parker \
+		        --role Admin \
+			    --email spiderman@superhero.org
+
+# start the web server, default port is 8080
+airflow webserver --port 8080
+
+# start the scheduler
+# open a new terminal or else run webserver with ``-D`` option to run it as a daemon
+airflow scheduler
+
+# visit localhost:8080 in the browser and use the admin account you just
+# created to login. Enable the example_bash_operator dag in the home page
