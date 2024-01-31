acca07caa34fd79fdc44eb730b195ab880eff973
Theja <tt@theja.org>
2024-01-31 14:11:00 -0600

    minor

--- docker_examples/docker_example/flask_simple_regression_service.py
+++ docker_examples/docker_example/flask_simple_regression_service.py
@@ -18,4 +18,4 @@ def predict():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=5001)
+    app.run(host="0.0.0.0",port=5002)
--- flask_examples/flask_example_regression/flask_simple_regression_service.py
+++ flask_examples/flask_example_regression/flask_simple_regression_service.py
@@ -1,30 +1,25 @@
-import pickle
 from flask import Flask, jsonify, request
+import flask
 
-
-def get_model(b, A):
-    def line(x):
-        return b * x + A
-    return line
-
-
-model_params = [2,2]
-model = get_model(model_params[0], model_params[1])
-
+def model(x):
+    return 2*x+1
 
 app = Flask(__name__)
 
-
 @app.route("/", methods=["GET"])
-def predict():
+def hello():
+    return f"flask version: {flask.__version__}"
 
+@app.route("/mypredict", methods=["GET"])
+def predict():
     if "x" in request.args:
         try:
-            return jsonify({'input': request.args['x'], 'prediction': model(float(request.args['x']))})
+            return jsonify({'input': request.args['x'],
+                             'prediction': model(float(request.args['x']))})
         except:
             pass
 
-    return jsonify({'success': 'false', 'message': 'Input x was not passed correctly.'})
+    return jsonify({'success': 'false', 'message': 'Input x was not passed.'})
 
 
 if __name__ == '__main__':
