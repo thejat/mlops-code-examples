1bab1c9ffbb2bcd721c61b690c26ee3fb9ee62ec
Ubuntu <theja@mlops-vm.ibggwu001ccezmi3k1av5sh1nd.bx.internal.cloudapp.net>
2025-02-05 22:03:12 +0000

    added docstring to a function in the flask example

--- flask_examples/flask_example_regression/flask_simple_regression_service.py
+++ flask_examples/flask_example_regression/flask_simple_regression_service.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 import flask
+import os
 
 def model(x):
     return 2*x+1
@@ -10,6 +11,19 @@ app = Flask(__name__)
 def hello():
     return f"flask version: {flask.__version__}"
 
+"""
+Endpoint to make a prediction based on the input parameter 'x'.
+
+This function handles GET requests to the /mypredict route. It checks if the 
+'x' parameter is present in the request arguments. If 'x' is present, it attempts 
+to convert 'x' to a float and pass it to the model for prediction. The result is 
+returned as a JSON response containing the input and the prediction. If 'x' is 
+not present or an error occurs during processing, an error message is returned.
+
+Returns:
+    Response: A JSON response containing the input and prediction if successful, 
+              or an error message if 'x' is not provided or an error occurs.
+"""
 @app.route("/mypredict", methods=["GET"])
 def predict():
     # check if x is in the arguments
@@ -24,4 +38,7 @@ def predict():
 
 
 if __name__ == '__main__':
-    app.run()
+    host = os.getenv('FLASK_RUN_HOST', '127.0.0.1')
+    port = int(os.getenv('FLASK_RUN_PORT', 5000))
+
+    app.run(host=host, port=port)
